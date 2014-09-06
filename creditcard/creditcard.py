47affb7d7479ceec686333079b5e3dfc344f2438
tffarrell-vz <farrell@abiresearch.com>
2014-09-06 12:03:29 -0400

    Drop last digit of a number.

--- creditcard/creditcard.py
+++ creditcard/creditcard.py
@@ -1,6 +1,11 @@
 
-
+cc_number = 4012888888881881
 def last_digit(number):
 	return number % 10
 	
 
+def drop_last_digit(number):
+	return number / 10
+
+print last_digit(cc_number)
+print drop_last_digit(cc_number)
